ac78e20065dac6b622c1a9d1bb996475537c8b8a
Yehor <yehor12344321@gmail.com>
2023-12-19 18:50:26 +0200

    зберігає зміни

---
+++ index.js
@@ -0,0 +1,46 @@
+// const tempCelsius = 4;
+// const tempFahrenheit = tempCelsius * 9 / 5 + 32;
+// console.log(tempFahrenheit);
+
+// const daysMonth = 31;
+// const hours = daysMonth * 24;
+// const minutes = hours * 60;
+
+// console.log(hours);
+// console.log(minutes);
+
+
+// let HP = 100; 
+// let stamina = 100;
+
+// HP -= 10;
+// stamina -= 20;
+
+// console.log(HP);
+// console.log(stamina);
+
+
+// let sum = 100; 
+// console.log(sum -= sum * 10 / 100);
+
+
+// let number = 124.45;
+
+// console.log(Math.floor(number)); 
+
+// const number = "1224.41";
+// console.log(Number.parseFloat(number)); 
+
+// const number = "1224px";
+// console.log(Number.parseInt(number));
+
+// const number = 9;
+// console.log(Math.sqrt(number));
+
+
+// const number = 12;
+// const text = "10rem";
+
+// console.log(typeof number.toString());
+// console.log(typeof Number.parseInt(text));
+
